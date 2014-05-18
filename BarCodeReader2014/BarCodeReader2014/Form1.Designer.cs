7d69a1c3f5bce1831696cb5be03f6e1506e2ed20
Voronin Igor <Voron.exe@gmail.com>
2014-05-18 05:04:21 +0300

    BarCodeWorking

--- BarCodeReader2014/BarCodeReader2014/Form1.Designer.cs
+++ BarCodeReader2014/BarCodeReader2014/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.PicBox = new System.Windows.Forms.PictureBox();
-            this.IdealBarCode = new System.Windows.Forms.PictureBox();
             this.DigitBox = new System.Windows.Forms.TextBox();
             this.BinaryBox = new System.Windows.Forms.TextBox();
             this.PathBox = new System.Windows.Forms.TextBox();
@@ -37,51 +36,48 @@
             this.DecodeBtn = new System.Windows.Forms.Button();
             this.OpenDlg = new System.Windows.Forms.OpenFileDialog();
             this.BrowseBtn = new System.Windows.Forms.Button();
+            this.PatterBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PicBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.IdealBarCode)).BeginInit();
             this.SuspendLayout();
             // 
             // PicBox
             // 
-            this.PicBox.Location = new System.Drawing.Point(323, 100);
+            this.PicBox.Location = new System.Drawing.Point(17, 67);
             this.PicBox.Name = "PicBox";
-            this.PicBox.Size = new System.Drawing.Size(727, 422);
+            this.PicBox.Size = new System.Drawing.Size(319, 132);
             this.PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PicBox.TabIndex = 0;
             this.PicBox.TabStop = false;
             // 
-            // IdealBarCode
-            // 
-            this.IdealBarCode.Location = new System.Drawing.Point(17, 224);
-            this.IdealBarCode.Name = "IdealBarCode";
-            this.IdealBarCode.Size = new System.Drawing.Size(300, 132);
-            this.IdealBarCode.TabIndex = 1;
-            this.IdealBarCode.TabStop = false;
-            // 
             // DigitBox
             // 
-            this.DigitBox.Location = new System.Drawing.Point(17, 198);
+            this.DigitBox.Location = new System.Drawing.Point(17, 284);
             this.DigitBox.Name = "DigitBox";
-            this.DigitBox.Size = new System.Drawing.Size(300, 20);
+            this.DigitBox.Size = new System.Drawing.Size(319, 20);
             this.DigitBox.TabIndex = 2;
             // 
             // BinaryBox
             // 
-            this.BinaryBox.Location = new System.Drawing.Point(17, 172);
+            this.BinaryBox.Location = new System.Drawing.Point(17, 258);
             this.BinaryBox.Name = "BinaryBox";
-            this.BinaryBox.Size = new System.Drawing.Size(300, 20);
+            this.BinaryBox.ReadOnly = true;
+            this.BinaryBox.Size = new System.Drawing.Size(319, 20);
             this.BinaryBox.TabIndex = 3;
             // 
             // PathBox
             // 
             this.PathBox.Location = new System.Drawing.Point(17, 12);
             this.PathBox.Name = "PathBox";
-            this.PathBox.Size = new System.Drawing.Size(300, 20);
+            this.PathBox.Size = new System.Drawing.Size(319, 20);
             this.PathBox.TabIndex = 4;
+            this.PathBox.TextChanged += new System.EventHandler(this.PathBox_TextChanged);
             // 
             // OpenBtn
             // 
-            this.OpenBtn.Location = new System.Drawing.Point(404, 10);
+            this.OpenBtn.Enabled = false;
+            this.OpenBtn.Location = new System.Drawing.Point(98, 38);
             this.OpenBtn.Name = "OpenBtn";
             this.OpenBtn.Size = new System.Drawing.Size(75, 23);
             this.OpenBtn.TabIndex = 5;
@@ -91,7 +87,7 @@
             // 
             // DecodeBtn
             // 
-            this.DecodeBtn.Location = new System.Drawing.Point(404, 38);
+            this.DecodeBtn.Location = new System.Drawing.Point(179, 38);
             this.DecodeBtn.Name = "DecodeBtn";
             this.DecodeBtn.Size = new System.Drawing.Size(75, 23);
             this.DecodeBtn.TabIndex = 6;
@@ -105,7 +101,7 @@
             // 
             // BrowseBtn
             // 
-            this.BrowseBtn.Location = new System.Drawing.Point(323, 10);
+            this.BrowseBtn.Location = new System.Drawing.Point(17, 38);
             this.BrowseBtn.Name = "BrowseBtn";
             this.BrowseBtn.Size = new System.Drawing.Size(75, 23);
             this.BrowseBtn.TabIndex = 7;
@@ -113,23 +109,51 @@
             this.BrowseBtn.UseVisualStyleBackColor = true;
             this.BrowseBtn.Click += new System.EventHandler(this.BrowseBtn_Click);
             // 
+            // PatterBox
+            // 
+            this.PatterBox.Location = new System.Drawing.Point(17, 232);
+            this.PatterBox.Name = "PatterBox";
+            this.PatterBox.ReadOnly = true;
+            this.PatterBox.Size = new System.Drawing.Size(319, 20);
+            this.PatterBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Выберите штрих код или введите свой";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(261, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Encode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1075, 587);
+            this.ClientSize = new System.Drawing.Size(354, 314);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PatterBox);
             this.Controls.Add(this.BrowseBtn);
             this.Controls.Add(this.DecodeBtn);
             this.Controls.Add(this.OpenBtn);
             this.Controls.Add(this.PathBox);
             this.Controls.Add(this.BinaryBox);
             this.Controls.Add(this.DigitBox);
-            this.Controls.Add(this.IdealBarCode);
             this.Controls.Add(this.PicBox);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.PicBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.IdealBarCode)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,7 +162,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox PicBox;
-        private System.Windows.Forms.PictureBox IdealBarCode;
         private System.Windows.Forms.TextBox DigitBox;
         private System.Windows.Forms.TextBox BinaryBox;
         private System.Windows.Forms.TextBox PathBox;
@@ -146,6 +169,9 @@
         private System.Windows.Forms.Button DecodeBtn;
         private System.Windows.Forms.OpenFileDialog OpenDlg;
         private System.Windows.Forms.Button BrowseBtn;
+        private System.Windows.Forms.TextBox PatterBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- BarCodeReader2014/BarCodeReader2014/Form1.cs
+++ BarCodeReader2014/BarCodeReader2014/Form1.cs
@@ -13,15 +13,27 @@ namespace BarCodeReader2014
     public partial class Form1 : Form
     {
         Bitmap Img;
+        Dictionary<string, number_type> encoding_table = new Dictionary<string, number_type>();
+        string[] A_codes = new string[10] { "0001101", "0011001", "0010011", "0111101", "0100011", "0110001",
+            "0101111","0111011","0110111","0001011"};
+        string[] first_number_code = new string[10]{"AAAAAA", "AABABB", "AABBAB", "AABBBA", "ABAABB", "ABBAAB", 
+                "ABBBAA", "ABABAB", "ABABBA", "ABBABA"};
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                encoding_table.Add(str_to_bool(A_codes[i], 'A'), new number_type(i, 'A'));
+                encoding_table.Add(str_to_bool(A_codes[i], 'B'), new number_type(i, 'B'));
+                encoding_table.Add(str_to_bool(A_codes[i], 'C'), new number_type(i, 'C'));
+            }
         }
 
         private void BrowseBtn_Click(object sender, EventArgs e)
         {
             DialogResult Res = OpenDlg.ShowDialog();
-            if (!Res.HasFlag(DialogResult.Cancel)){
+            if (!Res.HasFlag(DialogResult.Cancel))
+            {
                 PathBox.Text = OpenDlg.FileName;
             }
         }
@@ -37,15 +49,278 @@ namespace BarCodeReader2014
             return (c.R < 100 && c.B < 100 && c.G < 100);
         }
 
-        private void DecodeBtn_Click(object sender, EventArgs e)
+        struct number_type
         {
-
-            for (int xi = 0; xi < Img.Width; xi++)
+            public number_type(int n, char t)
             {
-               
+                number = n;
+                type = t;
+            }
+            public int number;
+            public char type;
+        }
+
+        private bool TryDecode(string full_bin_list, number_type[] n_type_array)
+        {
+            BinaryBox.Clear();
+            PatterBox.Clear();
+            for (int i = 0; i < 12; i++)
+            {
+                if (!encoding_table.TryGetValue(full_bin_list.Substring(i * 7, 7), out n_type_array[i]))
+                {
+                    label1.Text = "Error. There is no such bit patter in encoding table";
+                    return false;
+                }
+                BinaryBox.Text += full_bin_list.Substring(i * 7, 7) + " ";
             }
 
-            PicBox.Image = Img;
+            string encoded_type_str = "";
+            for (int i = 0; i < 6; i++)
+            {
+                encoded_type_str += n_type_array[i].type.ToString();
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                PatterBox.Text += n_type_array[i].type.ToString();
+            }
+
+            int first_number = -1;
+            for (int i = 0; i < 10; i++)
+            {
+                if (first_number_code[i] == encoded_type_str)
+                {
+                    first_number = i;
+                    break;
+                }
+            }
+            if (first_number == -1)
+            {
+                label1.Text = "Error. First number wasn't recognized";
+                return false;
+            }
+
+            string fullEAN = first_number.ToString();
+            for (int i = 0; i < 12; i++)
+            {
+                fullEAN += n_type_array[i].number.ToString();
+            }
+
+            DigitBox.Text = fullEAN;
+
+            int[] fullIntEAN = new int[13];
+            fullIntEAN[0] = first_number;
+            for (int i = 0; i < 12; i++)
+            {
+                fullIntEAN[i + 1] = n_type_array[i].number;
+            }
+            if (!checkCode(fullIntEAN))
+            {
+                label1.Text = "Error. Checksum is wrong";
+                return false;
+            }
+            return true;
+        }
+
+        private void DrawLine(int Pos, Bitmap b)
+        {
+            for (int y = 0; y < b.Height; y++)
+            {
+                for (int x = 5 * Pos; x < 5 * Pos + 5; x++)
+                {
+                    b.SetPixel(x, y, Color.Black);
+                }
+            }
+        }
+
+        private void Encode(string ean)
+        {
+            Bitmap Img2 = new Bitmap(485, 200);
+            using (Graphics g = Graphics.FromImage(Img2))
+            {
+                g.Clear(Color.White);
+                Pen p = new Pen(Color.Black, 5);
+
+                g.DrawLine(p, new PointF(7.5F, 0), new PointF(7.5F, 200));
+                g.DrawLine(p, new PointF(17.5F, 0), new PointF(17.5F, 200));
+            }
+            DrawLine(1, Img2);
+
+            PicBox.Image = Img2;
+        }
+
+        private void DecodeBtn_Click(object sender, EventArgs e)
+        {
+            if (Img == null) return;
+            List<List<int>> bb = new List<List<int>>();
+            for (int yi = 0; yi < Img.Height; yi++)
+            {
+                List<int> a = new List<int>();
+                int b = 1;
+                bool bc = IsBlack(Img.GetPixel(0, yi));
+                for (int xi = 1; xi < Img.Width; xi++)
+                {
+                    bool bc2 = IsBlack(Img.GetPixel(xi, yi));
+                    if (bc2 != bc)
+                    {
+                        a.Add(b);
+                        b = 0;
+                    }
+                    bc = bc2;
+                    b++;
+                }
+                bb.Add(a);
+            }
+
+            int[] cc = new int[255];
+            for (int i = 0; i < bb.Count; i++)
+            {
+                if (bb[i].Count < 255)
+                    cc[bb[i].Count]++;
+            }
+            int b_num = 0;
+            for (int i = 5; i < 255; i++)
+            {
+                if (cc[i] > b_num) b_num = i;
+            }
+            for (int i = 0; i < bb.Count; i++)
+            {
+                if (bb[i].Count != b_num)
+                {
+                    bb.RemoveAt(i);
+                    i--;
+                }
+            }
+            double b_len = 0;
+            foreach (List<int> a in bb)
+            {
+                b_len += a[1] + a[3] + a[a.Count - 2] + a[a.Count - 4];
+            }
+            b_len /= 4 * bb.Count;
+            b_len *= 0.88;     // це або баг або фіча,     
+            //довжина перших і останніх базових довжин стрічок трохи більша ніж треба
+
+            double w_len = 0;
+            foreach (List<int> a in bb)
+            {
+                w_len += a[2] + a[a.Count - 3];
+            }
+            w_len /= 2 * bb.Count;
+
+            int bb_inner_length = bb[0].Count;
+            string full_bin_list = "";
+            for (int i = 4; i <= bb_inner_length - 4; i++)
+            {
+                int cur_sum = 0;
+                foreach (List<int> cur_line in bb)
+                {
+                    cur_sum += cur_line[i];
+                }
+                int int_bits_amount = 0;
+
+                double average_length = (double)cur_sum / bb.Count;
+                if (i % 2 == 0)
+                {
+                    double bit_amount = average_length / w_len;
+                    int_bits_amount = (int)Math.Round(bit_amount);
+                    for (int j = 0; j < int_bits_amount; j++)
+                    {
+                        full_bin_list += "0";
+                    }
+                }
+                else
+                {
+                    double bit_amount = average_length / b_len;
+                    int_bits_amount = (int)Math.Round(bit_amount);
+                    for (int j = 0; j < int_bits_amount; j++)
+                    {
+                        full_bin_list += "1";
+                    }
+                }
+            }
+            full_bin_list = full_bin_list.Substring(0, 42) + full_bin_list.Substring(47, 42); //     Видалємо середні допоміжні біти
+
+
+            number_type[] n_type_array = new number_type[12];
+            label1.Text = "Успешно декодирован";
+            label1.ForeColor = Color.Green;
+            if (!TryDecode(full_bin_list, n_type_array))
+            {
+                number_type[] n_type_array2 = new number_type[12];
+                for (int i = 0; i < 12; i++)
+                {
+                    n_type_array2[11 - i] = n_type_array[i];
+                }
+                n_type_array = n_type_array2;
+                if (!TryDecode(full_bin_list, n_type_array))
+                {
+                    label1.ForeColor = Color.Red;
+                }
+            }
+
+
+        }
+
+
+        private bool checkCode(int[] source_digits)
+        {
+            int sum_evens = 0;
+            int sum_odds = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                if ((i % 2) == 1)
+                {
+                    sum_evens += source_digits[i];
+                }
+                else
+                {
+                    sum_odds += source_digits[i];
+                }                
+            }
+            int control_sum = (10 - ((sum_odds + 3 * sum_evens) % 10 )) % 10;
+            if (control_sum == source_digits[12])
+            {
+  //              MessageBox.Show("Control sum equals to " + control_sum.ToString() + "\nEAN code is confirmed.");
+                return true;
+            }
+            else
+            {
+  //              MessageBox.Show("Error. Control sum equals to " + control_sum.ToString() + "\nEAN code is wrong.");
+                return false;
+            }
+        }
+
+        private string str_to_bool(string source, char param)
+        {
+            if (param == 'A')
+                return source;
+            string res = "";
+            for (int i = 0; i < source.Length; i++)
+            {
+                char c;
+                if (source[i] == '0')
+                {
+                    c = '1';
+                }
+                else
+                {
+                    c = '0';
+                }
+                if (param == 'B')
+                    res = c.ToString() + res;
+                else if (param == 'C')
+                    res = res + c.ToString();
+            }
+            return res;
+        }
+
+        private void PathBox_TextChanged(object sender, EventArgs e)
+        {
+            OpenBtn.Enabled = System.IO.File.Exists(PathBox.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Encode("");
         }
 
     }
